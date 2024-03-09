3a22fd907a3c0eaa0525f2614f73872cdf1a9bff
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-03-09 04:29:03 -0600

    reading past end of queue throws exception

--- src/SPLORR.UI/CommandBuffer.vb
+++ src/SPLORR.UI/CommandBuffer.vb
@@ -1,9 +1,18 @@
 ﻿Public Class CommandBuffer(Of TCommand)
     Implements ICommandBuffer(Of TCommand)
+    Dim commands As New Queue(Of TCommand)
 
     Public ReadOnly Property HasCommand As Boolean Implements ICommandBuffer(Of TCommand).HasCommand
         Get
-            Return False
+            Return commands.Any
         End Get
     End Property
+
+    Public Sub WriteCommand(command As TCommand) Implements ICommandBuffer(Of TCommand).WriteCommand
+        commands.Enqueue(command)
+    End Sub
+
+    Public Function ReadCommand() As TCommand Implements ICommandBuffer(Of TCommand).ReadCommand
+        Return commands.Dequeue()
+    End Function
 End Class
--- src/SPLORR.UI/ICommandBuffer.vb
+++ src/SPLORR.UI/ICommandBuffer.vb
@@ -1,3 +1,5 @@
 ﻿Public Interface ICommandBuffer(Of TCommand)
     ReadOnly Property HasCommand As Boolean
+    Sub WriteCommand(command As TCommand)
+    Function ReadCommand() As TCommand
 End Interface
