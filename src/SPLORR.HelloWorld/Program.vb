4d3685b7d3be5f74e0fe25a6aad3c2155fc8d68e
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-03-11 05:24:43 -0500

    baseinput manager

--- src/SPLORR.HelloWorld/Program.vb
+++ src/SPLORR.HelloWorld/Program.vb
@@ -1,9 +1,21 @@
+Imports Microsoft.Xna.Framework.Input
+
 Module Program
     Sub Main(args As String())
         Using host As New Host(Of Hue, Command, Sfx, Model.Model)(
             New GameController(New HostConfig()),
             New Renderer(),
-            New InputManager(),
+            New BaseInputManager(Of Command)(New Dictionary(Of Command, Func(Of KeyboardState, GamePadState, Boolean)) From
+            {
+                {Command.A, Function(keyboard, gamePad) keyboard.IsKeyDown(Keys.Space) OrElse keyboard.IsKeyDown(Keys.Enter) OrElse gamePad.IsButtonDown(Buttons.A)},
+                {Command.B, Function(keyboard, gamePad) keyboard.IsKeyDown(Keys.Escape) OrElse keyboard.IsKeyDown(Keys.NumPad0) OrElse gamePad.IsButtonDown(Buttons.B)},
+                {Command.Up, Function(keyboard, gamePad) keyboard.IsKeyDown(Keys.Up) OrElse keyboard.IsKeyDown(Keys.NumPad8) OrElse gamePad.DPad.Up = ButtonState.Pressed},
+                {Command.Down, Function(keyboard, gamePad) keyboard.IsKeyDown(Keys.Down) OrElse keyboard.IsKeyDown(Keys.NumPad2) OrElse gamePad.DPad.Down = ButtonState.Pressed},
+                {Command.Left, Function(keyboard, gamePad) keyboard.IsKeyDown(Keys.Left) OrElse keyboard.IsKeyDown(Keys.NumPad4) OrElse gamePad.DPad.Left = ButtonState.Pressed},
+                {Command.Right, Function(keyboard, gamePad) keyboard.IsKeyDown(Keys.Right) OrElse keyboard.IsKeyDown(Keys.NumPad6) OrElse gamePad.DPad.Right = ButtonState.Pressed},
+                {Command.[Select], Function(keyboard, gamePad) keyboard.IsKeyDown(Keys.Tab) OrElse gamePad.IsButtonDown(Buttons.Back)},
+                {Command.Start, Function(keyboard, gamePad) keyboard.IsKeyDown(Keys.Enter) OrElse gamePad.IsButtonDown(Buttons.Start)}
+            }),
             New SfxManager())
             host.Run()
         End Using
---
+++ src/SPLORR.Presentation/BaseInputManager.vb
@@ -0,0 +1,32 @@
+﻿Public Class BaseInputManager(Of TCommand)
+    Implements IInputManager(Of TCommand)
+    Sub New(commandTable As IReadOnlyDictionary(Of TCommand, Func(Of KeyboardState, GamePadState, Boolean)))
+        Me.commandTable = commandTable
+    End Sub
+    Private ReadOnly commandTable As IReadOnlyDictionary(Of TCommand, Func(Of KeyboardState, GamePadState, Boolean))
+    Private ReadOnly nextCommandTimes As New Dictionary(Of TCommand, DateTimeOffset)
+    Public Function GetCommands(keyboardState As KeyboardState, gamePadState As GamePadState) As IEnumerable(Of TCommand) Implements IInputManager(Of TCommand).GetCommands
+        Dim result As New HashSet(Of TCommand)
+        For Each entry In commandTable
+            CheckForCommands(result, entry.Value(keyboardState, gamePadState), entry.Key)
+        Next
+        Return result.ToArray
+    End Function
+    Private Sub CheckForCommands(commands As HashSet(Of TCommand), isPressed As Boolean, command As TCommand)
+        If isPressed Then
+            Dim nextCommandTime As DateTimeOffset = Nothing
+            Dim currentCommandTime = DateTimeOffset.Now
+            If nextCommandTimes.TryGetValue(command, nextCommandTime) Then
+                If currentCommandTime > nextCommandTime Then
+                    commands.Add(command)
+                    nextCommandTimes(command) = currentCommandTime.AddSeconds(0.3)
+                End If
+            Else
+                commands.Add(command)
+                nextCommandTimes(command) = currentCommandTime.AddSeconds(1.0)
+            End If
+        Else
+            nextCommandTimes.Remove(command)
+        End If
+    End Sub
+End Class
