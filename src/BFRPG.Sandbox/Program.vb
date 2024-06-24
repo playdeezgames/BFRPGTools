85dafbe26bcee78cec9a5c35b94e23a31ccaeb8d
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-06-24 05:33:45 -0500

    i can delete a player

--- src/BFRPG.Sandbox/Program.vb
+++ src/BFRPG.Sandbox/Program.vb
@@ -7,15 +7,18 @@ Module Program
     Private Const ChoosePlayerText As String = "Choose Player... " & ChrW(8)
     Private Const QuitText As String = "Quit " & ChrW(8)
     Private Const GoBackText = "Go Back " & ChrW(8)
+    Private Const DeleteText = "Delete " & ChrW(8)
 
     Private Const MainMenuPrompt As String = "[olive]Main Menu[/]"
     Private Const NewPlayerNamePrompt As String = "[olive]New Player Name:[/]"
     Private Const PlayerMenuPrompt As String = "[olive]Player Menu:[/]"
 
     Private Const TablePlayers = "players"
+    Private Const ViewPlayerDetails = "player_details"
 
     Private Const ColumnPlayerName = "player_name"
     Private Const ColumnPlayerId = "player_id"
+    Private Const ColumnCharacterCount = "character_count"
 
     Sub Main(args As String())
         Using connection As New MySqlConnection(File.ReadAllText(ConnectionStringFilename))
@@ -34,6 +37,7 @@ Module Program
     Private Sub MainMenu(connection As MySqlConnection)
         Dim done = False
         While Not done
+            AnsiConsole.Clear()
             Dim prompt As New SelectionPrompt(Of String) With {.Title = MainMenuPrompt}
             prompt.AddChoice(NewPlayerText)
             prompt.AddChoice(ChoosePlayerText)
@@ -77,26 +81,44 @@ Module Program
     Private Sub PlayerMenu(connection As MySqlConnection, playerId As Integer)
         Dim done = False
         While Not done
+            AnsiConsole.Clear()
             Dim playerName As String
+            Dim characterCount As Integer
             Using command = connection.CreateCommand
-                command.CommandText = $"SELECT {ColumnPlayerName} FROM {TablePlayers} WHERE {ColumnPlayerId}=@{ColumnPlayerId};"
+                command.CommandText = $"SELECT {ColumnPlayerName},{ColumnCharacterCount} FROM {ViewPlayerDetails} WHERE {ColumnPlayerId}=@{ColumnPlayerId};"
                 command.Parameters.AddWithValue(ColumnPlayerId, playerId)
                 Using reader = command.ExecuteReader
                     If Not reader.Read Then
                         Return
                     End If
                     playerName = reader.GetString(0)
+                    characterCount = reader.GetInt32(1)
                 End Using
             End Using
             AnsiConsole.MarkupLine($"Player Id: {playerId}")
             AnsiConsole.MarkupLine($"Player Name: {playerName}")
+            AnsiConsole.MarkupLine($"Character Count: {characterCount}")
             Dim prompt As New SelectionPrompt(Of String) With {.Title = PlayerMenuPrompt}
             prompt.AddChoice(GoBackText)
+            If characterCount = 0 Then
+                prompt.AddChoice(DeleteText)
+            End If
             Select Case AnsiConsole.Prompt(prompt)
                 Case GoBackText
                     done = True
+                Case DeleteText
+                    DeletePlayer(connection, playerId)
+                    done = True
             End Select
         End While
+    End Sub
+
+    Private Sub DeletePlayer(connection As MySqlConnection, playerId As Integer)
+        Using command = connection.CreateCommand()
+            command.CommandText = $"DELETE FROM {TablePlayers} WHERE {ColumnPlayerId}=@{ColumnPlayerId};"
+            command.Parameters.AddWithValue(ColumnPlayerId, playerId)
+            command.ExecuteNonQuery()
+        End Using
     End Sub
 
     Private Sub CreatePlayer(connection As MySqlConnection)
